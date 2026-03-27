09bb3670cc601d38eaab3522e7fd2b6f55dce981
Sai_bhavan <47709071+saiinfy@users.noreply.github.com>
2026-03-27 14:46:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, krishna!</p>"
 
 if __name__ == '__main__':
     # Run the app on a specific port, for example, 8000
